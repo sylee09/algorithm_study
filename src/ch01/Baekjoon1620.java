c48892344aff733ee07d404564e6bd8e321a9c1a
Seungyoung Lee <leesy960909@gmail.com>
2025-11-02 16:10:05 +0900

    백준 1620번 문제풀이

---
+++ src/ch01/Baekjoon1620.java
@@ -0,0 +1,33 @@
+package ch01;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Baekjoon1620 {
+
+    public static void main(String[] args) {
+        // 입력 받기
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        HashMap<Integer, String> map1 = new HashMap<>();
+        HashMap<String, Integer> map2 = new HashMap<>();
+        sc.nextLine();
+
+        // map에 집어넣기
+        for (int i = 1; i <= N; i++) {
+            String pokemon = sc.nextLine();
+            map1.put(i, pokemon);
+            map2.put(pokemon, i);
+        }
+
+        for (int i = 1; i <= M; i++) {
+            String line = sc.nextLine();
+            if ((Character.isDigit(line.charAt(0)))) {
+                System.out.println(map1.get(Integer.parseInt(line)));
+            } else {
+                System.out.println(map2.get(line));
+            }
+        }
+    }
+}
