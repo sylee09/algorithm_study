919c3d69bd8a919dde924880b85ad293b0e6465e
Seungyoung Lee <leesy960909@gmail.com>
2025-11-15 22:45:58 +0900

    백준 19942번 문제풀이

--- src/ch04/Baekjoon19942.java
+++ src/ch04/Baekjoon19942.java
@@ -11,81 +11,77 @@ public class Baekjoon19942 {
         // 입력 받기
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[] condition = new int[4];
-
+        int[] conditionArr = new int[4];
         for (int i = 0; i < 4; i++) {
-            condition[i] = sc.nextInt();
+            conditionArr[i] = sc.nextInt();
         }
-
-        ArrayList<int[]> list = new ArrayList<>();
-
+        ArrayList<int[]> ingredients = new ArrayList<>();
         for (int i = 0; i < N; i++) {
-            int[] arr = new int[5];
+            int[] ingredient = new int[5];
             for (int j = 0; j < 5; j++) {
-                arr[j] = sc.nextInt();
+                ingredient[j] = sc.nextInt();
             }
-            list.add(arr);
+            ingredients.add(ingredient);
         }
 
         // 백트래킹
-        ArrayList<Integer> l = new ArrayList<>();
         for (int limit = 1; limit <= N; limit++) {
-            backtracking(condition, 0, list, l, limit);
+            backtracking(conditionArr, ingredients, new ArrayList<Integer>(), 0, limit);
         }
-        answer = answer == Integer.MAX_VALUE ? -1 : answer;
-        System.out.println(answer);
-        if (answer != -1) {
+        if (answer == Integer.MAX_VALUE) {
+            System.out.println(-1);
+        } else {
+            System.out.println(answer);
             for (int num : answerList) {
-                // 인덱스는 1부터
                 System.out.print(num + 1 + " ");
             }
         }
     }
 
-    static void backtracking(int[] condition, int startIdx, ArrayList<int[]> list, ArrayList<Integer> l, int limit) {
-        // 종료 조건
-        if (l.size() == limit) {
+    // 백트래킹: 만약 조건 만족하고 총 값이 같으면 사전순 비교하고 더 작은것 answerList에 넣기
+    // 총 값이 더 작다면 answer 갱신하고 answerList 갱신
+    static void backtracking(int[] conditionArr, ArrayList<int[]> ingredients, ArrayList<Integer> list, int index, int limit) {
+        if (list.size() == limit) {
             int a = 0;
             int b = 0;
             int c = 0;
             int d = 0;
             int cost = 0;
-            for (int num : l) {
-                a += list.get(num)[0];
-                b += list.get(num)[1];
-                c += list.get(num)[2];
-                d += list.get(num)[3];
-                cost += list.get(num)[4];
+            for (int idx : list) {
+                int[] ints = ingredients.get(idx);
+                a += ints[0];
+                b += ints[1];
+                c += ints[2];
+                d += ints[3];
+                cost += ints[4];
             }
-            if (a >= condition[0] && b >= condition[1] && c >= condition[2] && d >= condition[3] && cost <= answer) {
-                // 사전순 정렬을 위해
+
+            if (a >= conditionArr[0] && b >= conditionArr[1] && c >= conditionArr[2] && d >= conditionArr[3]) {
                 if (answer == cost) {
                     StringBuilder sb1 = new StringBuilder();
                     StringBuilder sb2 = new StringBuilder();
-                    for (int num : l) {
-                        sb1.append(num);
+                    for (int idx : list) {
+                        sb1.append(idx);
                     }
-                    for (int num : answerList) {
-                        sb2.append(num);
+                    for (int idx : answerList) {
+                        sb2.append(idx);
                     }
-                    // answerList보다 l이 사전순으로 빠르다면 answerList 갱신
                     if (sb1.toString().compareTo(sb2.toString()) < 0) {
                         answerList.clear();
-                        answerList.addAll(l);
+                        answerList.addAll(list);
                     }
-                } else {
-                    answer = cost;
+                } else if (answer > cost) {
                     answerList.clear();
-                    answerList.addAll(l);
+                    answerList.addAll(list);
+                    answer = cost;
                 }
             }
             return;
         }
-
-        for (int i = startIdx; i < list.size(); i++) {
-            l.add(i);
-            backtracking(condition, i + 1, list, l, limit);
-            l.remove(l.size() - 1);
+        for (int i = index; i < ingredients.size(); i++) {
+            list.add(i);
+            backtracking(conditionArr, ingredients, list, i + 1, limit);
+            list.remove(list.size() - 1);
         }
     }
 }
