e07eae387a868d51ceeb5c8ebcf2fc561460b6ce
Seungyoung Lee <leesy960909@gmail.com>
2025-11-26 12:21:11 +0900

    문자열 기본 문제들 풀이

---
+++ src/ch04_5/Baekjoon11654.java
@@ -0,0 +1,12 @@
+package ch04_5;
+
+import java.util.Scanner;
+
+public class Baekjoon11654 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        System.out.println((int) line.charAt(0));
+    }
+}
---
+++ src/ch04_5/Baekjoon11718.java
@@ -0,0 +1,13 @@
+package ch04_5;
+
+import java.util.Scanner;
+
+public class Baekjoon11718 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            System.out.println(sc.nextLine());
+        }
+    }
+}
