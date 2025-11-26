c06b13f0a481e9c22a484e83815ea4a18ef1a7b7
Seungyoung Lee <leesy960909@gmail.com>
2025-11-26 09:50:02 +0900

    반복문 기본 문제들 풀이

---
+++ src/ch04_5/Baekjoon15552.java
@@ -0,0 +1,24 @@
+package ch04_5;
+
+import java.io.*;
+
+public class Baekjoon15552 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < T; t++) {
+            String line = br.readLine();
+            String[] split = line.split(" ");
+            int A = Integer.parseInt(split[0]);
+            int B = Integer.parseInt(split[1]);
+            bw.append(A + B + "\n");
+        }
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
---
+++ src/ch04_5/Baekjoon25314.java
@@ -0,0 +1,20 @@
+package ch04_5;
+
+import java.util.Scanner;
+
+public class Baekjoon25314 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        int limit = N / 4;
+        for (int i = 0; i < limit; i++) {
+            sb.append("long ");
+        }
+
+        sb.append("int");
+
+        System.out.println(sb);
+    }
+}
