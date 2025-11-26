5fc516a127590534469c2afe900a4f598a1b16e5
Seungyoung Lee <leesy960909@gmail.com>
2025-11-26 15:44:18 +0900

    백준 심화 1단계 문제들 문제풀이

---
+++ src/ch04_5/Baekjoon25083.java
@@ -0,0 +1,12 @@
+package ch04_5;
+
+public class Baekjoon25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
---
+++ src/ch04_5/Baekjoon3003.java
@@ -0,0 +1,20 @@
+package ch04_5;
+
+import java.util.Scanner;
+
+public class Baekjoon3003 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 1, 2, 2, 2, 8};
+        int[] input = new int[6];
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 6; i++) {
+            input[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 6; i++) {
+            System.out.print(arr[i]-input[i]+" ");
+        }
+    }
+}
