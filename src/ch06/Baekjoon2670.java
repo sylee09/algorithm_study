aae13325ebefd590a71188f4892397317aa4350f
Seungyoung <leesy960909@gmail.com>
2025-12-29 15:20:59 +0900

    백준 2670 풀이

--- src/ch06/Baekjoon2670.java
+++ src/ch06/Baekjoon2670.java
@@ -3,26 +3,31 @@ package ch06;
 import java.util.Scanner;
 
 public class Baekjoon2670 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        double[] arr = new double[n];
 
-        for (int i = 0; i < n; i++) {
+    public static void main(String[] args) {
+        // 입력 받기
+        Scanner sc = new Scanner(System.in);
+        double max = 0;
+        int N = sc.nextInt();
+        double[] arr = new double[N];
+
+        for (int i = 0; i < N; i++) {
             arr[i] = sc.nextDouble();
         }
 
-        double answer = 0;
-
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < N; i++) {
             double num = arr[i];
-            answer = Math.max(num, answer);
-            for (int j = i + 1; j < n; j++) {
+            if (max < num) {
+                max = num;
+            }
+            for (int j = i + 1; j < N; j++) {
                 num *= arr[j];
-                answer = Math.max(num, answer);
+                if (max < num) {
+                    max = num;
+                }
             }
         }
 
-        System.out.printf("%.3f",answer);
+        System.out.printf("%.3f", max);
     }
 }
