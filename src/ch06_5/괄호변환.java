9e18a018ef3cf1125bdf8bec9601d2bbd9b9c003
Seungyoung <leesy960909@gmail.com>
2026-01-07 10:08:59 +0900

    괄호변환 문제풀이

---
+++ src/ch06_5/괄호변환.java
@@ -0,0 +1,72 @@
+package ch06_5;
+
+import java.util.Stack;
+
+public class 괄호변환 {
+
+    public String solution(String p) {
+        if (p.isEmpty()) {
+            return p;
+        }
+        return func(p);
+    }
+
+    public String func(String p) {
+        if (p.isEmpty()) {
+            return p;
+        }
+        int cnt1 = 0;
+        int cnt2 = 0;
+        int i=0;
+        for (; i < p.length(); i++) {
+            if (cnt1 > 0 && cnt1 == cnt2) {
+                break;
+            }
+            if (p.charAt(i) == '(') {
+                cnt1++;
+            } else {
+                cnt2++;
+            }
+        }
+
+        String u = p.substring(0, i);
+        String v = p.substring(i);
+
+        if (checkFunc(u)) {
+            return u + func(v);
+        } else {
+            String ret = "(" + solution(v) + ")";
+            StringBuilder sb = new StringBuilder(u);
+            if (sb.length() >= 1) {
+                sb.deleteCharAt(0);
+            }
+            if (sb.length() >= 1) {
+                sb.deleteCharAt(sb.length()-1);
+            }
+            StringBuilder str = new StringBuilder();
+            for (char ch : sb.toString().toCharArray()) {
+                if (ch == '(') {
+                    str.append(")");
+                } else {
+                    str.append("(");
+                }
+            }
+            return ret + str.toString();
+        }
+    }
+
+    public boolean checkFunc(String u) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : u.toCharArray()) {
+            if (ch == '(') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+}
