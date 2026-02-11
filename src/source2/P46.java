923c417a84e291e035abfd01e1541d7f9cf95418
Seungyoung <leesy960909@gmail.com>
2026-02-11 09:57:24 +0900

    삽입 정렬 문제풀이

---
+++ src/source2/P46.java
@@ -0,0 +1,32 @@
+package source2;
+
+import java.util.Scanner;
+
+public class P46 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+
+        for (int element : arr) {
+            System.out.print(element + " ");
+        }
+    }
+}
