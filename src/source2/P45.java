aeda8dd3c4a2596f72c931e02d8bc884ec21f7a7
Seungyoung <leesy960909@gmail.com>
2026-02-11 09:50:05 +0900

    버블 정렬 문제 풀이

---
+++ src/source2/P45.java
@@ -0,0 +1,34 @@
+package source2;
+
+import java.util.Scanner;
+
+public class P45 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (arr[i] < arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        for (int element : arr) {
+            System.out.print(element+" ");
+        }
+    }
+}
