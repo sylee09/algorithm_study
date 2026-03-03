fe9709c0cfa8171a86058dd4476c9c1f712651aa
Seungyoung <leesy960909@gmail.com>
2026-03-03 12:55:34 +0900

    계단오르기 문제풀이

---
+++ src/source2/P72.java
@@ -0,0 +1,23 @@
+package source2;
+
+import java.util.Scanner;
+
+public class P72 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] dp = new int[N + 1];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        System.out.println(dp[N]);
+    }
+}
