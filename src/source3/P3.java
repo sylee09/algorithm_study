a5414004ac53418ff8c62b4de332f584ddd4046b
Seungyoung <leesy960909@gmail.com>
2026-03-30 21:05:22 +0900

    잃어버린 강아지 문제풀이

--- src/source3/P3.java
+++ src/source3/P3.java
@@ -2,65 +2,65 @@ package source3;
 
 import java.util.ArrayDeque;
 
-public class P3 {
+import java.util.*;
+class P3 {
     static int[] dx = new int[]{-1, 0, 1, 0};
     static int[] dy = new int[]{0, 1, 0, -1};
 
-    public int solution(int[][] board) {
-
+    public int solution(int[][] board){
         ArrayDeque<int[]> deque1 = new ArrayDeque<>();
         ArrayDeque<int[]> deque2 = new ArrayDeque<>();
 
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
                 if (board[i][j] == 2) {
-                    deque1.offer(new int[]{i, j, 0});
+                    deque1.add(new int[]{i, j});
                 }
                 if (board[i][j] == 3) {
-                    deque2.offer(new int[]{i, j, 0});
+                    deque2.add(new int[]{i, j});
                 }
             }
         }
 
-        int curTime = 0;
+        int time = 0;
+        int dir1 = 0;
+        int dir2 = 0;
 
-        while (curTime < 10_000) {
-            int[] cur = deque1.poll();
-            int[] puppy = deque2.poll();
+        while (time < 10_000) {
+            int[] cur1 = deque1.poll();
+            int[] cur2 = deque2.poll();
 
-            if (cur[0] == puppy[0] && cur[1] == puppy[1]) {
-                return curTime;
+            if (cur1[0] == cur2[0] && cur1[1] == cur2[1]) {
+                return time;
             }
 
-            // 현수 이동
-            int dirCur = cur[2];
-            int nx = cur[0] + dx[dirCur];
-            int ny = cur[1] + dy[dirCur];
+            int nx1 = cur1[0] + dx[dir1];
+            int ny1 = cur1[1] + dy[dir1];
 
-            if (nx < 0 || nx >= board.length || ny < 0 || ny >= board[0].length || board[nx][ny] == 1) {
-                dirCur = (dirCur + 1) % 4;
-                deque1.offer(new int[]{cur[0], cur[1], dirCur});
+            if (nx1 < 0 || nx1 > 9 || ny1 < 0 || ny1 > 9 || board[nx1][ny1] == 1) {
+                dir1 = (dir1 + 1) % 4;
+                deque1.add(cur1);
             } else {
-                deque1.offer(new int[]{nx, ny, dirCur});
+                deque1.add(new int[]{nx1, ny1});
             }
 
-            // 강아지 이동
-            int dirPuppy = puppy[2];
-            int nextPuppyX = puppy[0] + dx[dirPuppy];
-            int nextPuppyY = puppy[1] + dy[dirPuppy];
+            int nx2 = cur2[0] + dx[dir2];
+            int ny2 = cur2[1] + dy[dir2];
 
-            if (nextPuppyX < 0 || nextPuppyX >= board.length || nextPuppyY < 0 || nextPuppyY >= board[0].length || board[nextPuppyX][nextPuppyY] == 1) {
-                dirPuppy = (dirPuppy + 1) % 4;
-                deque2.offer(new int[]{puppy[0], puppy[1], dirPuppy});
+            if (nx2 < 0 || nx2 > 9 || ny2 < 0 || ny2 > 9 || board[nx2][ny2] == 1) {
+                dir2 = (dir2 + 1) % 4;
+                deque2.add(cur2);
             } else {
-                deque2.offer(new int[]{nextPuppyX, nextPuppyY, dirPuppy});
+                deque2.add(new int[]{nx2, ny2});
             }
-            curTime++;
+
+            time++;
         }
+
         return 0;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         P3 T = new P3();
         int[][] arr1 = {{0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
                 {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
