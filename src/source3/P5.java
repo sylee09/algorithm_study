586da5c4bb3d900985e8d69fd6f361165436c511
Seungyoung <leesy960909@gmail.com>
2026-03-30 22:07:30 +0900

    최대길이 바이토닉 수열 문제풀이

--- src/source3/P5.java
+++ src/source3/P5.java
@@ -1,37 +1,33 @@
 package source3;
 
-import java.util.*;
+
+import java.util.ArrayList;
 
 class P5 {
     public int solution(int[] nums){
-        ArrayList<Integer> peakIndex = new ArrayList<>();
+        ArrayList<Integer> idxList = new ArrayList<>();
 
         for (int i = 1; i < nums.length - 1; i++) {
-            if (nums[i - 1] < nums[i] && nums[i] > nums[i + 1]) {
-                peakIndex.add(i);
+            if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
+                idxList.add(i);
             }
         }
 
         int answer = 0;
-
-        for (int element : peakIndex) {
-            int count = 1;
-            int left = element - 1;
-            int cur = nums[element];
-            while (left >= 0 && cur > nums[left]) {
+        for (int idx : idxList) {
+            int left = idx - 1;
+            int right = idx + 1;
+            int cur = nums[idx];
+            while (left >= 0 && nums[left] < cur) {
                 cur = nums[left];
                 left--;
-                count++;
             }
-            int right = element + 1;
-            cur = nums[element];
-            while (right < nums.length && cur > nums[right]) {
+            cur = nums[idx];
+            while (right < nums.length && nums[right] < cur) {
                 cur = nums[right];
                 right++;
-                count++;
             }
-
-            answer = Math.max(answer, count);
+            answer = Math.max(answer, right - left - 1);
         }
 
         return answer;
@@ -39,7 +35,7 @@ class P5 {
 
     public static void main(String[] args){
         P5 T = new P5();
-        System.out.println(T.solution(new int[]{1, 2, 1, 2, 3, 2, 1}));
+        System.out.println(T.solution(new int[]{1, 3, 2, 5, 7, 4, 2, 5, 1}));
         System.out.println(T.solution(new int[]{1, 1, 2, 3, 5, 7, 4, 3, 1, 2}));
         System.out.println(T.solution(new int[]{3, 2, 1, 3, 2, 4, 6, 7, 3, 1}));
         System.out.println(T.solution(new int[]{1, 3, 1, 2, 1, 5, 3, 2, 1, 1}));
