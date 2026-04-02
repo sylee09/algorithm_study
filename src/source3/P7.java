010b0c7866245bb24066cb6b635a805ef622e19b
Seungyoung <leesy960909@gmail.com>
2026-04-02 19:35:00 +0900

    비밀번호 문제풀이

--- src/source3/P7.java
+++ src/source3/P7.java
@@ -1,66 +1,55 @@
 package source3;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 class P7 {
-    static int[] dx = new int[]{-1, 0, 1, 0, -1, 1, 1, -1};
-    static int[] dy = new int[]{0, 1, 0, -1, 1, 1, -1, -1};
+    static int[] dx = new int[]{-1, -1, 0, 1, 1, 1, 0, -1};
+    static int[] dy = new int[]{0, 1, 1, 1, 0, -1, -1, -1};
 
     public int solution(int[] keypad, String password){
-        int[][] dist = new int[11][11];
-        for (int i = 0; i <= 10; i++) {
-            Arrays.fill(dist[i], 2);
-        }
-
-        for (int i = 1; i <= 10; i++) dist[i][i] = 0;
-
-        int[][] arr = new int[3][3];
-
+        HashMap<String, int[]> map = new HashMap<>();
+        int idx = 0;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                arr[i][j] = keypad[i * 3 + j];
+                int key = keypad[idx++];
+                map.put(String.valueOf(key), new int[]{i, j});
             }
         }
 
-        for (int k = 0; k < dx.length; k++) {
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    int nx = dx[k] + i;
-                    int ny = dy[k] + j;
-
-                    if (nx >= 0 && nx < 3 && ny >= 0 && ny < 3) {
-                        int from = arr[i][j];
-                        int to = arr[nx][ny];
-
-                        dist[from][to] = 1;
-                        dist[to][from] = 1;
-                    }
-                }
-            }
-        }
-
+        int[] cur = map.get(String.valueOf(password.charAt(0)));
         int answer = 0;
-        int from = Integer.parseInt(String.valueOf(password.charAt(0)));
 
         for (int i = 1; i < password.length(); i++) {
-            int to = Integer.parseInt(String.valueOf(password.charAt(i)));
-            answer += dist[from][to];
-            from = to;
+            String p = String.valueOf(password.charAt(i));
+            int[] next = map.get(p);
+            int curX = cur[0];
+            int curY = cur[1];
+
+            int nextX = next[0];
+            int nextY = next[1];
+
+            if (curX == nextX && curY == nextY) {
+                continue;
+            }
+
+            boolean found = false;
+            for (int k = 0; k < dx.length; k++) {
+                if (curX + dx[k] == nextX && curY + dy[k] == nextY) {
+                    answer += 1;
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                answer += 2;
+            }
+
+            cur = next;
         }
 
         return answer;
-
-    }
-
-    public int[] findIndex(char element, int[][] arr) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (arr[i][j] == Integer.parseInt(String.valueOf(element))) {
-                    return new int[]{i, j};
-                }
-            }
-        }
-        return null;
     }
 
     public static void main(String[] args){
