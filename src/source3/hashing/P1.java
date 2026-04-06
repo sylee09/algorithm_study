348249c98be3822ff016492db1e1555b7b01507f
Seungyoung <leesy960909@gmail.com>
2026-04-06 19:17:34 +0900

    한번 사용한 최초 문자 문제풀이

---
+++ src/source3/hashing/P1.java
@@ -0,0 +1,27 @@
+package source3.hashing;
+
+import java.util.HashMap;
+
+class P1 {
+    public int solution(String s){
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1) {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args){
+        P1 T = new P1();
+        System.out.println(T.solution("statitsics"));
+        System.out.println(T.solution("aabb"));
+        System.out.println(T.solution("stringshowtime"));
+        System.out.println(T.solution("abcdeabcdfg"));
+    }
+}
