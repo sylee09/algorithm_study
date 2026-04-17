5c1189de5b13f55db2c0168a1edbbe984c0c1576
Seungyoung <leesy960909@gmail.com>
2026-04-17 23:20:34 +0900

    문제2 배열 제어하기 문제풀이

---
+++ src/book1/Ch5_2.java
@@ -0,0 +1,21 @@
+package book1;
+
+import java.util.Arrays;
+import java.util.TreeSet;
+
+public class Ch5_2 {
+    private int[] solution(int[] arr) {
+        TreeSet<Integer> set = new TreeSet<>((a, b) -> b.compareTo(a));
+        for (int element : arr) {
+            set.add(element);
+        }
+
+        return set.stream().mapToInt(a -> a.intValue()).toArray();
+    }
+
+    public static void main(String[] args) {
+        Ch5_2 solution = new Ch5_2();
+        System.out.println(Arrays.toString(solution.solution(new int[]{4, 2, 2, 1, 3, 4})));
+        System.out.println(Arrays.toString(solution.solution(new int[]{2, 1, 1, 3, 2, 5, 4})));
+    }
+}
