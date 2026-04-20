85895e20ecb2772d8d098c118ee5d2ede3e5275f
Seungyoung <leesy960909@gmail.com>
2026-04-20 23:26:20 +0900

    두 개 뽑아서 더하기 문제풀이

---
+++ src/book1/Ch5_3.java
@@ -0,0 +1,28 @@
+package book1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Ch5_3 {
+
+    public int[] solution(int[] numbers) {
+        ArrayList<Integer> answerList = new ArrayList<>();
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                int sum = numbers[i] + numbers[j];
+                answerList.add(sum);
+            }
+        }
+        answerList.sort((a, b) -> a.compareTo(b));
+
+        return answerList.stream().mapToInt(x -> x.intValue()).distinct().toArray();
+    }
+
+    public static void main(String[] args) {
+        Ch5_3 solution = new Ch5_3();
+        int[] numbers = new int[]{2, 1, 3, 4, 1};
+
+        int[] answer = solution.solution(numbers);
+        System.out.println(Arrays.toString(answer));
+    }
+}
