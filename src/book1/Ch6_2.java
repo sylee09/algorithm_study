b68406d77f521f95d20014ef10c1575d1f06890f
Seungyoung <leesy960909@gmail.com>
2026-04-26 18:19:13 +0900

    10진수를 2진수로 변환하기 문제풀이

---
+++ src/book1/Ch6_2.java
@@ -0,0 +1,25 @@
+package book1;
+
+import java.util.Stack;
+
+public class Ch6_2 {
+    String solution(int decimal) {
+        Stack<Integer> stack = new Stack<>();
+        while (decimal > 0) {
+            stack.push(decimal % 2);
+            decimal /= 2;
+        }
+        StringBuilder ans = new StringBuilder();
+        while (!stack.isEmpty()) {
+            ans.append(stack.pop());
+        }
+        return ans.toString();
+    }
+
+    public static void main(String[] args) {
+        Ch6_2 solution = new Ch6_2();
+        System.out.println(solution.solution(10));
+        System.out.println(solution.solution(27));
+        System.out.println(solution.solution(12345));
+    }
+}
